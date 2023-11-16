cc803686139173dafe26eed633df15887b6b17fc
Josh Katz <josh@xqmsg.com>
2023-11-16 15:53:01 -0500

    using fyne for GUI

--- cmd/ui/main.go
+++ cmd/ui/main.go
@@ -1,48 +1,39 @@
 package main
 
 import (
-	"image/color"
-	"log"
-	"os"
-
-	"gioui.org/app"
-	"gioui.org/io/system"
-	"gioui.org/layout"
-	"gioui.org/op"
-	"gioui.org/text"
-	"gioui.org/widget/material"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
+	"github.com/joshuaskatz/imageproc/internal/image"
+	//"fyne.io/fyne/v2/theme"
 )
 
 func main() {
-	go func() {
-		w := app.NewWindow()
-		err := run(w)
-		if err != nil {
-			log.Fatal(err)
-		}
-		os.Exit(0)
-	}()
-	app.Main()
-}
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Button Widget")
+	var uri string
+	content := widget.NewButton("click me", func() {
+		d := dialog.NewFileOpen(func(f fyne.URIReadCloser, e error) {
+			uri = f.URI().Path()
 
-func run(w *app.Window) error {
-	th := material.NewTheme()
-	var ops op.Ops
-	for {
-		e := <-w.Events()
-		switch e := e.(type) {
-		case system.DestroyEvent:
-			return e.Err
-		case system.FrameEvent:
-			gtx := layout.NewContext(&ops, e)
+			file := image.NewFile()
 
-			title := material.H1(th, "Hello, Gio")
-			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
-			title.Color = maroon
-			title.Alignment = text.Middle
-			title.Layout(gtx)
+			file.SetFilePath(uri)
 
-			e.Frame(gtx.Ops)
-		}
-	}
+			file.LoadImage()
+
+			file.SetIsColor(true)
+
+			file.Conversion()
+
+			file.SaveImage()
+
+		}, myWindow)
+
+		d.Show()
+	})
+
+	myWindow.SetContent(content)
+	myWindow.ShowAndRun()
 }
